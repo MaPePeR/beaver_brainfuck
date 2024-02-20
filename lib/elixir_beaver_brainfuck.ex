e5657bf37c73d32ce9f2b524cf943070e0e37883
MaPePeR <MaPePeR@users.noreply.github.com>
2024-02-20 18:56:20 +0000

    Empty project with `mix new` project and elixir devcontainer

---
+++ lib/elixir_beaver_brainfuck.ex
@@ -0,0 +1,18 @@
+defmodule ElixirBeaverBrainfuck do
+  @moduledoc """
+  Documentation for `ElixirBeaverBrainfuck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirBeaverBrainfuck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_beaver_brainfuck_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirBeaverBrainfuckTest do
+  use ExUnit.Case
+  doctest ElixirBeaverBrainfuck
+
+  test "greets the world" do
+    assert ElixirBeaverBrainfuck.hello() == :world
+  end
+end
